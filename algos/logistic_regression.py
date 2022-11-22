3d563edbca714a857823b8d51a1bbdfe0faef3ef
auhide <bo0opr@gmail.com>
2022-11-22 15:38:02 +0200

    Fixed LogisticRegression; Added a base Notebook implementation

--- algos/logistic_regression.py
+++ algos/logistic_regression.py
@@ -9,9 +9,9 @@ class LogisticRegression(Model):
     def __init__(self):
         self.w = None
 
-    def fit(self, X: torch.Tensor, y: torch.Tensor, l_rate=1e-2, iters=100):
+    def fit(self, X: torch.Tensor, y: torch.Tensor, l_rate=1e-2, iters=100, random_state=42):
         # Defining the weights with random normally distributed values
-        self.w = torch.randn(size=(X.shape[1], 1), requires_grad=True)
+        self.w = torch.randn(size=(X.shape[1], 1), requires_grad=True, random_state=random_state)
 
         # Training for 'iters' iterations
         for _ in tqdm(range(iters)):
@@ -21,6 +21,8 @@ class LogisticRegression(Model):
             with torch.no_grad():
                 self.w -= l_rate * self.w.grad
                 self.w.grad.zero_()
+
+        print(f"Loss: {loss.item():.2f}")
 
     def predict(self, X: torch.Tensor):
         return self._sigmoid(X @ self.w)
@@ -32,15 +34,14 @@ class LogisticRegression(Model):
         return self._log_likelihood_loss(y_pred, y)
 
     def _sigmoid(self, x: torch.Tensor):
-        return 1 / (1 + torch.exp(x))
+        return 1 / (1 + torch.exp(-x))
 
     def _log_likelihood_loss(self, y_pred, y):
-        # We are going to divide scale_factor, which is equal to 
-        # the number of samples/experiments.
-        scale_factor = 1 / y_pred.shape[0]
-        loss = torch.sum(
+        # scale_factor is equal to 1 over the number of samples/experiments.
+        scale_factor = 1 / len(y)
+        loss = - scale_factor * torch.sum(
             (y * torch.log(y_pred) + (1 - y) * torch.log(1 - y_pred))
-        ) / scale_factor
+        )
 
         return loss
 
@@ -54,3 +55,7 @@ if __name__ == "__main__":
 
     regressor = LogisticRegression()
     regressor.fit(X, y)
+
+    print("X shape", X.shape)
+    print("y shape", y.shape)
+    print("y_pred shape:", regressor.predict(X).shape)
